eb6ba5d3060c0d553509f4be44f1807b031982ea
Znina-bilel <bilelzn13@gmail.com>
2024-12-17 18:54:01 +0100

    mon portfolio

---
+++ src/const/const.js
@@ -0,0 +1,168 @@
+export const studies = [
+  {
+    title: "Licence en Developpement Web et Multimedia",
+    corporation: "ISET - Nabeul (Tunisie)",
+    date: " 06/24",
+  },
+  {
+    title: "Developpeur Web (Stagaire)",
+    corporation: "SRTGN - Nabeul (Tunisie) ",
+    date: "01/24 – 02/24",
+  },
+  {
+    title: "Developpeur Mobile(Stagaire)",
+    corporation: "SRTGN - Nabeul (Tunisie)",
+    date: "02/24 – 06/24",
+  },
+  
+];
+
+export const IconKeys = {
+  Php: "php",
+  html: "html",
+  css: "css",
+  javaScript: "javaScript",
+  react: "react",
+  astro: "astro",
+  materialUi: "materialUi",
+  nextjs: "nextjs",
+  redux: "redux",
+  styled: "styled",
+  tailwind: "tailwind",
+  radix: "radix",
+  reactQuery: "reactQuery",
+  typeScript: "typeScript",
+  zustand: "zustand",
+  git: "git",
+  github: "github",
+  stripe: "stripe",
+  shadcn: "shadcn",
+  nodejs: "nodejs",
+  mongodb: "mongodb",
+  vite: "vite",
+  // here add more icon keys
+};
+
+export const skills = [
+  {
+    title: "Flutter",
+    icon: IconKeys.Flutter
+  },
+  {
+    title: "Php",
+    icon: IconKeys.Php
+  },
+  {
+    title: "Html",
+    icon: IconKeys.html,
+  },
+  {
+    title: "Css",
+    icon: IconKeys.css,
+  },
+  {
+    title: "JavaScript",
+    icon: IconKeys.javaScript,
+  },
+  {
+    title: "Astro",
+    icon: IconKeys.astro,
+  },
+  {
+    title: "Tailwindcss",
+    icon: IconKeys.tailwind,
+  },
+  {
+    title: "React Js",
+    icon: IconKeys.react,
+  },
+ 
+  {
+    title: "Git",
+    icon: IconKeys.git,
+  },
+
+  {
+    title: "Github",
+    icon: IconKeys.github,
+  },
+  {
+    title: "Material UI",
+    icon: IconKeys.materialUi,
+  },
+
+ 
+  
+  {
+    title: "Node Js",
+    icon: IconKeys.nodejs,
+  },
+  {
+    title: "TypeScript",
+    icon: IconKeys.typeScript,
+  },
+  
+  
+
+  
+  {
+    title: "Mongo Db",
+    icon: IconKeys.mongodb,
+  },
+];
+
+export const projects = [
+  {
+    img: "../../../img/appweb.jpg",
+    title: "SRTGN WEB APP",
+    link: "https://github.com/Znina-bilel/SRTGN_Web_App",
+    description: `J'ai développé une application web pendant mon stage au sein de la SRTGN, visant à faciliter la gestion des transports publics dans la région de Nabeul. Cette application permet aux utilisateurs de consulter les horaires et de suivre les trajets des bus en temps réel. Elle a été conçue pour améliorer l'expérience des usagers grâce à une interface simple et intuitive ."`,
+    technologies: [
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.javaScript,
+      },
+      {
+        icon: IconKeys.Php
+      },
+     
+    ],
+  },
+  {
+    img: "../../../img/mobile.jpg",
+    title: "SRTGN Mobile App",
+    link: "https://github.com/Znina-bilel/SRTGN_MOBILE",
+    description: `J'ai créé une application mobile pendant mon stage au SRTGN, conçue pour faciliter les déplacements des voyageurs dans la région de Nabeul. L'application permet de consulter les horaires des bus, suivre les trajets en temps réel et obtenir des informations pratiques. Elle vise à offrir une expérience fluide et accessible aux usagers du transport public.  `,
+    technologies: [
+      {
+      icon: IconKeys.materialUi,
+      },
+      {
+        icon: IconKeys.react,
+      }
+    ],
+  },
+  {
+    img: "../../../img/crud.jpg",
+    title: "Laravel CRUD",
+    link: "https://github.com/Znina-bilel/Laravel-crud",
+    description: `plataforma de anime para ver los animes en tendencia , más valorados , estadísticas.`,
+    technologies: [
+      {
+        icon: IconKeys.html,
+      },
+      {
+        icon: IconKeys.css,
+      },
+      {
+        icon: IconKeys.Php,
+      },
+      
+    ],
+  },
+];
